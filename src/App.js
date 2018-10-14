dcbc6b39fc6201f9fea78e7ac50669d429da3bcf
Pavlos Vinieratos <pvinis@gmail.com>
2018-10-14 19:35:50 +0200

    header name done

--- src/App.js
+++ src/App.js
@@ -6,53 +6,48 @@ import {
 import {
   Text,
 } from './components'
+import { Colors } from './ui'
 
+import me from './me'
 
-// TODO: put this in a file
-const pavlos = {
-  name: {
-    first: 'Pavlos',
-    second: 'Vinieratos',
-  },
-  position: 'Software Developer \u00b7 Software Engineer',
-  location: 'Amsterdam, Netherlands',
-  quote: 'I enjoy experimenting with new technologies, competing in coding contests and working with like-minded people.',
-  contact: {
-    phone: '+31 6-2723-9822',
-    github: 'pvinis',
-    stackoverflow: { id: '377456', name: 'pvinis' },
-    gitlab: 'pvinis',
-  },
-}
-
-const me = pavlos
 
 export default
 class App extends React.Component {
   render() {
     return (
       <View
-      style={{
-        flexDirection: 'row',
-        marginTop: 10, 
-        justifyContent: 'center',
-        // display: flex
-        // flex-direction: ${props => props.horizontal ? 'row' : 'column'}
-        // align-items: ${props => props.centered ? 'center' : null}
-        // justify-content: ${props => (props.centered && props.horizontal) ? 'center' : null}
-      }}
+        style={{
+          flexDirection: 'row',
+          marginTop: '0.6cm',
+          justifyContent: 'center',
+        }}
       >
-        <Text
-        style={{
-          fontSize: 30
-        }}
-        >{me.name.first} </Text>
-        <Text bold
-        style={{
-          fontSize: 30
-        }}
-        >{me.name.second}</Text>
+        <Text style={styles.name}>
+          <Text style={styles.nameFirst}>
+            {me.name.first} 
+          </Text>
+          {' '}
+          <Text style={styles.nameSecond}>
+            {me.name.second}
+          </Text>
+        </Text>
       </View>
     )
   }
+}
+ 
+const styles = {
+  name: {
+    fontFamily: 'Roboto',
+    fontSize: '32pt',
+    marginBottom: '0.4mm',
+  },
+  nameFirst: {
+    fontWeight: 100,
+    color: Colors.textGray,
+  },
+  nameSecond: {
+    fontWeight: 'bold',
+    color: Colors.text,
+  },
 }
---
+++ src/me.js
@@ -0,0 +1,17 @@
+const me = {
+  name: {
+    first: 'Pavlos',
+    second: 'Vinieratos',
+  },
+  position: 'Software Developer \u00b7 Software Engineer',
+  location: 'Rotterdam, Netherlands',
+  quote: 'I enjoy experimenting with new technologies, competing in coding contests and working with like-minded people.',
+  contact: {
+    phone: '+31 6-2723-9822',
+    github: 'pvinis',
+    stackoverflow: { id: '377456', name: 'pvinis' },
+    gitlab: 'pvinis',
+  },
+}
+
+export default me
