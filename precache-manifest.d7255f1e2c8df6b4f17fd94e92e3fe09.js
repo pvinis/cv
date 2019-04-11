89e5dc1de66b0bc55e35a2dbd32e71a4e20d5b71
Pavlos Vinieratos <pvinis@gmail.com>
2019-04-11 08:05:43 +0200

    Updates

---
+++ precache-manifest.d7255f1e2c8df6b4f17fd94e92e3fe09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6fc4cc2cbc109e9ecb2",
+    "url": "/cv/static/css/main.9be90ee6.chunk.css"
+  },
+  {
+    "revision": "e6fc4cc2cbc109e9ecb2",
+    "url": "/cv/static/js/main.03f29119.chunk.js"
+  },
+  {
+    "revision": "0a60133db753f53aac51",
+    "url": "/cv/static/js/runtime~main.35a64a98.js"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/cv/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "956334e47b1efd20358a8195134fa72f",
+    "url": "/cv/index.html"
+  }
+];
